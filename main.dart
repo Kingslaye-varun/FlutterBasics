d6229fd3e74674e5820989e6753cbd4b3d0de352
Kingslaye-varun <52694208+Kingslaye-varun@users.noreply.github.com>
2024-09-30 06:27:25 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,120 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'next_page.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyAppExt(),
+    );
+  }
+}
+
+class MyAppExt extends StatefulWidget {
+  const MyAppExt({super.key});
+
+  @override
+  State<MyAppExt> createState() => _MyAppExtState();
+}
+
+class _MyAppExtState extends State<MyAppExt> {
+  String buttonName = 'Click';
+  int currentIndex = 0;
+  bool _isClicked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App Title'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.brown,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.orange,
+                      ),
+                      onPressed: () {
+                        setState(
+                          () {
+                            buttonName = 'Clicked';
+                          },
+                        );
+                      },
+                      child: Text(buttonName),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) => const NextPage(),
+                          ),
+                        );
+                      },
+                      child: const Text('Next Page'),
+                    ),
+                  ],
+                ),
+              )
+            : GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isClicked = !_isClicked;
+                  });
+                },
+                child: _isClicked
+                    ? Image.asset(
+                        'images/Appicon.jpeg')
+                    : Image.network(
+                        'https://images.icc-cricket.com/image/upload/t_ratio21_9-size60/prd/spldcm6oih92w5jmwtnq',
+                      ),
+              ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Settings',
+            icon: Icon(Icons.settings),
+          )
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
